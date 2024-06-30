91fd43403fe3473b27fa0e93edc2f97ad697a404
NotteShock <lekruistz@gmail.com>
2024-06-30 14:52:49 +0000

    sus

--- tailwind.config.js
+++ tailwind.config.js
@@ -68,6 +68,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        figtree: ['Figtree', 'system-ui'],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
